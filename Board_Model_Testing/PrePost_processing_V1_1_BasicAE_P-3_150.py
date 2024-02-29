86cb7a73f53962bc5d7ffeac740df43efd8777fd
Jake O'Brien <113451770+jaketobrien@users.noreply.github.com>
2024-02-29 10:22:47 +0000

    Update PrePost_processing_V1_1_BasicAE_P-3_150.py

--- Board_Model_Testing/PrePost_processing_V1_1_BasicAE_P-3_150.py
+++ Board_Model_Testing/PrePost_processing_V1_1_BasicAE_P-3_150.py
@@ -274,10 +274,7 @@ for output_detail in output_details:
     output_data = interpreter.get_tensor(output_detail['index'])
     output_data_list.append(output_data)
 
-X_train_np = np.array(X_train)
-#X_val = X_train[:int(len(X_train)/20)]
-X_val = X_train_np[:int(len(X_train_np)/20)].astype(np.float32)
-X_val = X_val.reshape(1, -1)
+X_val = X_train[:int(len(X_train)/20)]
 
 # Perform inference
 interpreter.set_tensor(input_details[0]['index'], X_val)
